12f758ce5f3d7b34660237d94aa45caf5f33e472
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-15 00:43:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,14 @@
-class Anagram       
-  attr_accessor :word
+class Anagram       #----> defines class that we are working with 
+  attr_accessor :word    #----> we are working with words, to set your getter and setter method to #word, then we can work with any word
   
-  def initialize(word)
+  def initialize(word)  #---> initializes word, this just needs to be habit
     @word = word
  
   end
   
-  def match(array)
-    array.select do |x|
-      x.split("").sort == word.split("").sort
+  def match(array)  #--->create a new method that passes an argument of whatever the spec file says.
+    array.select do |x|  #----> here we are iterating over an array of words that this method is taking in
+      x.split("").sort == word.split("").sort #---> this method will split the word into "w", "o", "r", "d" and sort them      
       end
     end
 end
